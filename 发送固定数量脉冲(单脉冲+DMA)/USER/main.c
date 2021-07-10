e869996eed5ebc56d8005704260b1f7a6cab6de0
yatt <1254452743@qq.com>
2021-07-10 11:08:06 +0800

    修改LED亮度

--- 发送固定数量脉冲(单脉冲+DMA)/USER/main.c
+++ 发送固定数量脉冲(单脉冲+DMA)/USER/main.c
@@ -33,7 +33,7 @@ int main(void)
 	tim3_dma_pulse_Output(10,9,40,5);
 	while(1) {
 		ITM_SendChar('B');
-		tim3_dma_pulse_Output(2000,7200,50,5);
+		tim3_dma_pulse_Output(2000,7200,10,5);
 		delay_ms(1000);
 		delay_ms(1000);
 	}
